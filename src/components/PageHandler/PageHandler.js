88b7cbb382fa0399148b8b91ca91ced05c451e51
William Felippe F. da Silva <wfelippesilva@gmail.com>
2017-09-18 23:24:12 -0300

    feat: Adding a component to render the state of page
    A component where is verified the status of page and render a loader, an error message or the component passed as children element

---
+++ src/components/PageHandler/PageHandler.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Loader, Error } from '../'
+
+const PageHandler = (props) => {
+
+    const { loading, error, errorMessage, children } = props
+
+    if (error) {
+        return (
+            <Error errorMessage={errorMessage} />
+        )
+    }
+    else {
+        return (
+            <div>
+                {loading && <Loader />}
+                {children}
+            </div>
+        )
+    }
+}
+
+PageHandler.defaultProps = {
+    loading: false,
+    error: false
+}
+
+PageHandler.propTypes = {
+    children: PropTypes.element.isRequired,
+    loading: PropTypes.bool,
+    error: PropTypes.bool
+}
+
+export default PageHandler

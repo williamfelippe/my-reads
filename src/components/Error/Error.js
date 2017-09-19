1ad84ce9d0fe010e63a5c1143c47907d1bffede7
William Felippe F. da Silva <wfelippesilva@gmail.com>
2017-09-18 23:22:10 -0300

    feat: Adding a component to render an crucial error message

---
+++ src/components/Error/Error.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Row, Col, Icon, Button } from 'react-materialize'
+import './style.css'
+
+const Error = () => {
+    return (
+        <div className="error">
+            <Row className="error__container">
+                <Col s={12}>
+                    <Icon className="error__icon">
+                        mood_bad
+                    </Icon>
+                </Col>
+
+                <Col s={12}>
+                    <p className="error__message">
+                        Ocorreu um erro inexperado
+                    </p>
+                </Col>
+
+                <Col s={12}>
+                    <Button 
+                        waves='light' 
+                        className="error__button" 
+                        onClick={() => window.location.reload()}>
+                        Tentar novamente
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Error

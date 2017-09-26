a7e0a8951aeb48d58483319b410c42211799b84e
William Felippe F. da Silva <wfelippesilva@gmail.com>
2017-09-25 22:43:43 -0300

    refactor: Changing component NoMatch to a stateless component

--- src/containers/NoMatch/NoMatch.js
+++ src/containers/NoMatch/NoMatch.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './style.css'
 
-class NoMatch extends Component {
-    render() {
-        return (
-            <div className="nomatch">
-                <h1 className="nomatch__title">
-                    404
-                </h1>
+const NoMatch = () => {
+    return (
+        <div className="nomatch">
+            <h1 className="nomatch__title">
+                404
+            </h1>
 
-                <p className="nomatch__description">
-                    Ops... Essa página não existe
-                </p>
-            </div>
-        )
-    }
+            <p className="nomatch__description">
+                Ops... Essa página não existe
+            </p>
+        </div>
+    )
 }
 
 export default NoMatch
